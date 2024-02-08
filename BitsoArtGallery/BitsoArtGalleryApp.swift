9819ff7b63e7523c6c44784a3b1cae1becb10b92
Iñaki <inayabarb@gmail.com>
2024-02-08 12:40:10 -0300

    Initial Commit

---
+++ BitsoArtGallery/BitsoArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitsoArtGalleryApp.swift
+//  BitsoArtGallery
+//
+//  Created by Iñaki Yabar Bilbao on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BitsoArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
